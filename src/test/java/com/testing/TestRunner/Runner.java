e8e30df05312937e9502c0c89530b08637a66b13
Jayaranjitha <jaya29593@gmail.com>
2020-08-02 20:46:09 +0530

    Change pom

--- src/test/java/com/testing/TestRunner/Runner.java
+++ src/test/java/com/testing/TestRunner/Runner.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 
 @CucumberOptions(features = {"src/test/resources/Features"}, glue= {"com.test.java.stepDefinitions"}, tags="@ThoughtWorksChk", plugin = {
-        "pretty", "json:target/surefire-reports/cucumber.json"})
+        "pretty", "html:target/cucumber", "json:target/cucumber/cucumber.json"})
 public class Runner 				
 {		
 
